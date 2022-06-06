9c598c2ff11d5f27a6788cbfab106f2b2f30fb65
00041275 <ADILBEKQ@halykbank.kz>
2022-06-06 13:35:28 +0600

    Доработан отчет - font понимание Roboto

--- src/main/java/barakat/app/report/Report.java
+++ src/main/java/barakat/app/report/Report.java
@@ -6,19 +6,31 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.fonts.SimpleFontExtensionsRegistryFactory;
+import net.sf.jasperreports.engine.xml.JRFontFactory;
 import net.sf.jasperreports.export.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.support.Repositories;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @Log4j2
+@Configuration
 public class Report {
+
+
 
     @Value("${report.path}")
     String path;
@@ -26,16 +38,20 @@ public class Report {
     @Value("${report.outputFolder}")
     String outputFolder;
 
-    @Bean
-    public void demo2(DataSource appDataSource) throws JasperException {
+    @Autowired
+    DataSource appDataSource;
+
+    public String demo2() throws JasperException {
+
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put("start", new Timestamp(2019, 1, 18, 0, 0,0,0));
+        parameters.put("start", Timestamp.valueOf(LocalDateTime.parse("2022-04-05T10:10")));
         parameters.put("end", new Timestamp(2022, 4, 18, 0, 0,0,0));
-        generatePdfReport(appDataSource,"report1", parameters);
+        parameters.put("projectPath", "report/MyReports/");
+        generatePdfReport("report1", parameters);
+        return "ddd";
     }
 
-
-    public void generatePdfReport(DataSource appDataSource, String reportName, Map<String, Object> parameters) throws JasperException {
+    public void generatePdfReport(String reportName, Map<String, Object> parameters) throws JasperException {
         try {
             JasperReport jasperReport = JasperCompileManager
                     .compileReport(path +"/" + reportName + ".jrxml");
@@ -45,6 +61,7 @@ public class Report {
             File outDir = new File(outputFolder);
             outDir.mkdirs();
             // PDF Exportor.
+
             JRPdfExporter exporter = new JRPdfExporter();
             ExporterInput exporterInput = new SimpleExporterInput(jasperPrint);
             // ExporterInput
